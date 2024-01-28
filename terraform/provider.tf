bd29cc1308393c1e7b9835c28a06e65306278302
Scandr <yaitskaya-helen@yandex.ru>
2024-01-28 23:16:14 +0300

    terraform version update to v1.6.3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,24 +5,32 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+  required_version = ">= 0.13"
+
   backend "s3" {
-  endpoint   = "storage.yandexcloud.net"
+#  endpoint   = "storage.yandexcloud.net"
+#  endpoints.s3  = "storage.yandexcloud.net"
+  endpoints = {
+    s3 = "https://storage.yandexcloud.net"
+  }
   bucket     = "netology-diploma"
-  region     = "ru-central"
+  region     = "ru-central1"
   key        = "netology-diploma/terraform.tfstate"
 # $ yc iam access-key create --service-account-name terraform
 #  access_key = "" # key_id:
 #  secret_key = "" # secret:
-#  skip_region_validation      = true
-#  skip_credentials_validation = true
-#  skip_requesting_account_id  = true
-
   skip_region_validation      = true
   skip_credentials_validation = true
+  skip_requesting_account_id  = true
+
+#  skip_region_validation      = true
+#  skip_credentials_validation = true
   }
 }
 
 provider "yandex" {
+  zone = "ru-central1-b"
+#  token =
   service_account_key_file = "key.json"
   cloud_id  = "${var.yandex_cloud_id}"
   folder_id = "${var.yandex_folder_id}"
